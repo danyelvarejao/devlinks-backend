bd429abf357b03ea22ed56d399f32af990947c15
danyelvarejao <danyelvarejao@gmail.com>
2024-03-31 20:32:30 -0300

    test(e2e): add login

---
+++ backend/src/http/controllers/auth/login.spec.ts
@@ -0,0 +1,41 @@
+import request from 'supertest'
+import { it, describe, expect, beforeAll, afterAll } from 'vitest'
+
+import { app } from '@/app'
+import { HttpStatusCode } from '@/http/utils'
+
+describe('Login (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to login', async () => {
+    await request(app.server).post('/users').send({
+      email: 'any-email@email.com',
+      password: '123456'
+    })
+
+    const response = await request(app.server).post('/login').send({
+      email: 'any-email@email.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(HttpStatusCode.OK)
+    expect(response.body).toEqual({
+      token: expect.any(String)
+    })
+  })
+
+  it('should not be able to create a user with an invalid email', async () => {
+    const response = await request(app.server).post('/users').send({
+      email: 'any-email',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(HttpStatusCode.BAD_REQUEST)
+  })
+})
