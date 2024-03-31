e0d0d9f7d3475619de9ab7af49d998ebd8865d59
danyelvarejao <danyelvarejao@gmail.com>
2024-03-31 20:19:05 -0300

    test: generate new database for each e2e tests

--- backend/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ backend/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,39 @@
+import 'dotenv/config'
+import { execSync } from 'child_process'
 import { type Environment } from 'vitest'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const generateDatabaseURL = (schema: string): string => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 const environment: Environment = {
   name: 'prisma',
   transformMode: 'web',
 
   async setup() {
-    console.log('Setup')
+    const schema = crypto.randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+        await prisma.$disconnect()
       }
     }
   }
